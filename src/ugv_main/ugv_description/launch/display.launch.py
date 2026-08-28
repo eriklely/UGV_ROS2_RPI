0ac7d57fd62eb3b644684b67723fc4fab021b4ea
eriklely <eriklely@hotmail.com>
2026-08-28 15:03:23 +0200

    2

--- src/ugv_main/ugv_description/launch/display.launch.py
+++ src/ugv_main/ugv_description/launch/display.launch.py
@@ -1,6 +1,6 @@
 import os
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
